199be7e093da9222278111d96e3bc04cb179c475
TotallyDIO <totallydio@proton.me>
2026-06-19 16:19:04 -0500

    updated testRevisiom, made functions, called them

--- wiiEmulator/testRevision.sh
+++ wiiEmulator/testRevision.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-# Ensure the script is run with sufficient privileges to see block devices
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root or with sudo to ensure all USB devices are detected."
-  exit 1
-fi
+# Ver 0.1
+# (GNU/General Public License version 3.0)
+# by TotallyDIO @ https://github.com/AurumOS
+# ----------------------------------------
+# Define Variablesa
+# ----------------------------------------
+rootfs=$(pwd)/mount/aurumos/
+bootfs=$(pwd)/mount/boot/
 
+testroot () {
+  if [[ "$EUID" = 0 ]]; then
+    continue
+  else
+    echo "Please Run As Root"
+    sleep 2
+    exit
+  fi
+}
+
+findusbs () {
 echo "Scanning for connected USB storage devices..."
 echo "------------------------------------------------"
 
@@ -46,12 +60,13 @@ done
 echo "------------------------------------------------"
 echo "Success! You selected: $DEVICE"
 clear
+}
 
+partitionusb () {
 # starts fdisk to partition the device
 echo "starting fdisk utility for $DEVICE..."
 sudo /sbin/fdisk $DEVICE
 sleep 2
-
 # make new dos partition table
 echo "o \n"
 # starts new partition
@@ -67,7 +82,6 @@ echo "+256M \n"
 echo "t \n"
 #changes fs to fat16
 echo "6 \n"
-
 #new partition
 echo "n \n"
 #primary partition
@@ -77,7 +91,6 @@ echo "2 \n"
 echo "\n"
 #space allocation
 echo "+1600M /n"
-
 #new partition
 echo "n \n"
 #primary
@@ -93,7 +106,55 @@ echo "3 \n"
 echo "82 \n"
 #writes to disk
 echo "w \n"
-
 sudo /sbin/mkfs.vfat -n boot $DEVICE"1"
 sudo /sbin/mkfs.ext2 -L aurum $DEVICE"2"
 sudo /sbin/mkswap $DEVICE"3"
+}
+
+mountusb () {
+# creates mount points & mounts partitoions
+sudo mkdir ./mount/boot
+sudo mount $DEVICE"1" ./mount/boot
+
+sudo mkdir ./mount/aurumos
+sudo mount $DEVICE"2" ./mount/aurumos
+}
+
+installbase () {
+mkdir -p $(pwd)/var/lib/pacman
+
+#installs base packages
+pacman --root $rootfs --cachedir $rootfs/var/cache/pacman/pkg --config $(pwd)/pacman.conf -b $rootfs/var/lib/pacman -Sy
+pacman --root $rootfs --cachedir $rootfs/var/cache/pacman/pkg --config $(pwd)/pacman.conf -b $rootfs/var/lib/pacman -S base base-devel openfwwf
+sudo cp $(pwd)pacman.conf $rootfs/etc/pacman.conf
+}
+
+createfiles () {
+    # create fstab
+touch $rootfs/etc/fstab
+echo "
+/dev/$DEVICE"2"          /            ext2      defaults,noatime            0 1
+/dev/$DEVICE"1"          /boot        vfat      defaults,noatime            0 1
+
+tmpfs                   /var/log     tmpfs     size=16m                    0 0" > $rootfs/etc/fstab
+
+touch $rootfs/etc/rc.conf
+echo "HOSTNAME=aurumos" > $rootfs/etc/rc.conf
+
+touch $rootfs/etc/hosts
+echo "127.0.0.1      archlinux.domain.org   localhost.localdomain      localhost    archlinux" > $rootfs/etc/hosts
+}
+
+umntclnup () {
+sudo umount $bootfs
+sudo umount $rootfs
+cp $rootfs $(pwd)
+}
+
+testroot
+findusbs
+partitionusb
+mountusb
+installbase
+createfiles
+umntclnup
